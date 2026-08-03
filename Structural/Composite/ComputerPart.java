c62d0d24eac6a59de02f0717854ab3695ed05298
SadMan ZaMaN <sadmanzaman88@gmail.com>
2026-08-04 02:27:20 +0600

    newlyadded

--- Structural/Composite/ComputerPart.java
+++ Structural/Composite/ComputerPart.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 interface component{
     void showPrice();
+    int getPrice();
 }
 
 class Leaf implements component{
@@ -18,6 +19,11 @@ class Leaf implements component{
     @Override
     public void showPrice() {
         System.out.println("Leaf : " + name + " Price: " + price);
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
     }
 }
 
@@ -40,6 +46,15 @@ class Composite implements component{
         for (component c : components) {
             c.showPrice();
         }
+    }
+
+    @Override
+    public int getPrice() {             // This method calculates the total price of all components in the composite. It iterates through each component in the list and sums their prices to return the total price of the composite.
+        int totalPrice = 0;
+        for (component c : components) {
+            totalPrice += c.getPrice();
+        }
+        return totalPrice;
     }
 }
 
